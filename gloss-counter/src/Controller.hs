440cd601f015e75256dbb396160e3d2fbad4ce8d
7340796 <t.w.westland@students.uu.nl>
2025-10-14 10:03:00 +0200

    Player acceleration

--- gloss-counter/src/Controller.hs
+++ gloss-counter/src/Controller.hs
@@ -19,15 +19,6 @@ step secs gstate
 input :: Event -> GameState -> IO GameState
 input e gstate = return (handleInput e gstate)
 
---Maps a key to an action
-inputKey :: Key -> GameState -> GameState
-inputKey k gstate
-  = case k of 
-    (Char 'w') -> gstate {player = updatePlayerPosition (playerAccelerate (player gstate))} 
-    (Char 'a') -> gstate {player = (player gstate){playerDirection = steerLeft (player gstate)}} 
-    (Char 'd') -> gstate {player = (player gstate){playerDirection = steerRight (player gstate)}}
-    _ -> gstate --other characters do not trigger movement
-
 --Add a key to the set of pressed keys when a key is pressed. Removes when key is released.
 handleInput :: Event -> GameState -> GameState
 handleInput (EventKey k Down _ _) gstate = gstate {keys = S.insert k (keys gstate)}
@@ -36,24 +27,29 @@ handleInput _ gstate                     = gstate
 
 --Go through the list of pressed keys and update the gamestate accordingly
 update :: GameState -> GameState
-update gstate | keys gstate == S.empty = baseState
-              | otherwise              = foldr f e (keys gstate)
-  where
-    f k acc       | S.member k set && k == Char 'w' = inputKey k baseState
-                  | S.member k set = inputKey k acc
-                  | otherwise      = baseState
-    e             = gstate 
-    set           = S.fromList [Char 'w', Char 'a', Char 'd']
-    baseState     = gstate {player = updatePlayerPosition (playerSlowDown (player gstate))}
+update gstate =  turn (keys gstate) (moveForward (keys gstate) gstate)
+moveForward :: S.Set Key -> GameState -> GameState
+moveForward s gstate | S.member (Char 'w') s = gstate{player = updatePlayerPosition (playerAccelerate (player gstate))}
+                     | otherwise           = gstate{player = updatePlayerPosition (playerSlowDown (player gstate))}
 
---Update the player position
+turn :: S.Set Key -> GameState -> GameState
+turn s gstate = foldr turnHelper gstate s
 
+turnHelper :: Key -> GameState -> GameState
+turnHelper k gstate = 
+  case k of
+    (Char 'a') -> gstate {player = (player gstate){playerDirection = steerLeft (player gstate)}} 
+    (Char 'd') -> gstate {player = (player gstate){playerDirection = steerRight (player gstate)}}
+    _ -> gstate
+
+--Set the player speed when slowing down
 playerSlowDown :: Player -> Player
 playerSlowDown  p@(Player {playerSpeed = v}) = p{playerSpeed = newSpeed}
   where
-    newSpeed | v > 0     = v - 0.1
+    newSpeed | v > 0     = v - 0.15
              | otherwise = 0
 
+--Set the player speed when speeding up
 playerAccelerate :: Player -> Player
 playerAccelerate p@(Player {playerSpeed = v}) = p{playerSpeed = newSpeed}
   where 
@@ -61,6 +57,7 @@ playerAccelerate p@(Player {playerSpeed = v}) = p{playerSpeed = newSpeed}
                    | otherwise = v
     maxSpeed       = 10 
 
+--Set the new player position
 updatePlayerPosition :: Player -> Player
 updatePlayerPosition p@(Player {playerDirection = Angle a, playerSpeed = v, playerPosition = Point x y}) = p{playerPosition = newPosition} 
   where 
