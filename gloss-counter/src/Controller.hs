48876fcb160ce0790dc003dd7d1404a31d597adf
Alicia Torre van Schouwen <a.f.torrevanschouwen@students.uu.nl>
2025-10-14 09:44:16 +0200

    acc

--- gloss-counter/src/Controller.hs
+++ gloss-counter/src/Controller.hs
@@ -26,27 +26,32 @@ inputKey k gstate
     (Char 'w') -> gstate {player = updatePlayerPosition (playerAccelerate (player gstate))} 
     (Char 'a') -> gstate {player = (player gstate){playerDirection = steerLeft (player gstate)}} 
     (Char 'd') -> gstate {player = (player gstate){playerDirection = steerRight (player gstate)}}
-    _ -> gstate {player = updatePlayerPosition (playerSlowDown (player gstate))} --other characters do not trigger movement
+    _ -> gstate --other characters do not trigger movement
 
 --Add a key to the set of pressed keys when a key is pressed. Removes when key is released.
 handleInput :: Event -> GameState -> GameState
-handleInput (EventKey k Down _ _) gstate = gstate { keys = S.insert k (keys gstate)}
+handleInput (EventKey k Down _ _) gstate = gstate {keys = S.insert k (keys gstate)}
 handleInput (EventKey k Up _ _) gstate   = gstate {keys = S.delete k (keys gstate)}
 handleInput _ gstate                     = gstate
 
 --Go through the list of pressed keys and update the gamestate accordingly
 update :: GameState -> GameState
-update gstate = foldr f e (keys gstate)
-  where 
-    f         = inputKey
-    e         = gstate 
+update gstate | keys gstate == S.empty = baseState
+              | otherwise              = foldr f e (keys gstate)
+  where
+    f k acc       | S.member k set && k == Char 'w' = inputKey k baseState
+                  | S.member k set = inputKey k acc
+                  | otherwise      = baseState
+    e             = gstate 
+    set           = S.fromList [Char 'w', Char 'a', Char 'd']
+    baseState     = gstate {player = updatePlayerPosition (playerSlowDown (player gstate))}
 
 --Update the player position
 
 playerSlowDown :: Player -> Player
 playerSlowDown  p@(Player {playerSpeed = v}) = p{playerSpeed = newSpeed}
   where
-    newSpeed | v > 0     = v - acceleration p
+    newSpeed | v > 0     = v - 0.1
              | otherwise = 0
 
 playerAccelerate :: Player -> Player
